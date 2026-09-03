7e1d4ed06fc5df183ad2fc3f78957bbd7399aa1f
Heer Chauhan <heerc1555@gmail.com>
2026-09-03 11:53:45 +0530

    Update app (4).py

--- app (4).py
+++ app (4).py
@@ -160,7 +160,7 @@ st.divider()
 st.subheader("🔢 Manual Rate Lookup")
 
 if loan_type == "Home Loan":
-    min_tenure, max_tenure = 5, 25
+    min_tenure, max_tenure = 2, 25
 else:
     min_tenure, max_tenure = 2, 10
 
